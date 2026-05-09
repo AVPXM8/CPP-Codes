179a72cc15bc5b3de2b351b327929add190753fd
AVPXM8 <113023480+AVPXM8@users.noreply.github.com>
2026-05-09 16:04:50 +0530

    Updated Student Classe

--- OOPS/studentClass.cpp
+++ OOPS/studentClass.cpp
@@ -1,27 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-class Student{
-	// private data members 
-	public:
-	int rollNo;
-	string name;
-	string inClass;
-	int age;
-	int nos;
-	// attribuite and beahviour of the class
-	void study(){
-		cout<<" Studying "<<endl;
-		
-	}
-	void sleep(){
-		cout<< " the student is sleeping "<<endl;
-	}
-	
+class Student {
+private:
+    int rollNo;
+    string name;
+    string inClass;
+    int age;
+
+public:
+    Student(int roll, string n, string c, int a) 
+        : rollNo(roll), name(n), inClass(c), age(a) {}
+
+    void setName(string n) { name = n; }
+    string getName() const { return name; }
+
+    void study() {
+        cout << name << " is studying." << endl;
+    }
+
+    void sleep() {
+        cout << name << " is sleeping." << endl;
+    }
 };
-int main(){
-	Student obj;
-	obj.study();
-	obj.name="vivek ";
-	cout<<obj.name;
-}
+
+int main() {
+    Student obj(101, "Vivek", "12th", 18);
+    obj.study();
+    obj.setName("Vivek Kumar");
+    cout << "Student Name: " << obj.getName() << endl;
+    return 0;
+}
